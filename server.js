f270228cde24748432ffb8a73e148553c18f7eb9
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-01-12 11:30:31 -0600

    server routes

--- server.js
+++ server.js
@@ -6,6 +6,31 @@ var PORT = 3000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json);
 
+var res = [];
+var tab= [];
+
+app.get("/", function(request, response) {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
+
+app.get("/reservations", function(request, response) {
+    res.sendFile(path.join(__dirname, "reservations.html"))
+});
+
+app.get("/tables", function(request, response) {
+    res.sendFile(path.join(__dirname, "tables.html"))
+});
+
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
