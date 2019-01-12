f92359435d2bb6fcf2e462e9a788a90ff318d774
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-01-12 11:47:00 -0600

    changes

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.json);
+app.use(express.json());
 
 var reservations = [];
 var tables= [];
 
 app.get("/", function(request, response) {
+   
     response.sendFile(path.join(__dirname, "index.html"))
 });
 
 app.get("/reservations", function(request, response) {
-    response.sendFile(path.join(__dirname, "reservations.html"))
+    response.sendFile(path.join(__dirname, "/reservations.html"))
 });
 
 app.get("/tables", function(request, response) {
